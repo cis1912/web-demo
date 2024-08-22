c96a613792729d6cb4692deba664d15a779fe0eb
Eunsoo Shin <me@esinx.net>
2024-08-22 16:52:36 -0400

    with pydantic and uuid4

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from typing import Annotated
-
-from fastapi import FastAPI, HTTPException, Body
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from uuid import uuid4
+
+
+class Task(BaseModel):
+    id: str
+    task: str
+    completed: bool = False
+
+
+class TaskCreate(BaseModel):
+    task: str
+
 
 app = FastAPI()
 
@@ -17,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-tasks = []
+tasks: list[Task] = []
 
 
 @app.get("/tasks")
@@ -26,18 +38,14 @@ def get_tasks():
 
 
 @app.post("/tasks")
-def create_task(task: Annotated[str, Body(embed=True)]):
-    tasks.append({
-        "id": len(tasks),
-        "task": task,
-        "completed": False,
-    })
+def create_task(task: TaskCreate):
+    tasks.append(Task(id=str(uuid4()), task=task.task, completed=False))
     return tasks
 
 
 @app.delete("/tasks/{id}")
-def delete_task(id: int):
-    matches = [i for i, t in enumerate(tasks) if t["id"] == id]
+def delete_task(id: str):
+    matches = [i for i, t in enumerate(tasks) if t.id == id]
     if len(matches) == 0:
         raise HTTPException(status_code=404, detail="Task not found")
     idx = matches[0]
@@ -46,16 +54,10 @@ def delete_task(id: int):
 
 
 @app.put("/tasks/{id}")
-def update_task(id: int,
-                task: Annotated[str, Body(embed=True)],
-                completed: Annotated[bool, Body(embed=True)]):
-    matches = [i for i, t in enumerate(tasks) if t["id"] == id]
+def update_task(task: Task):
+    matches = [i for i, t in enumerate(tasks) if t.id == task.id]
     if len(matches) == 0:
         raise HTTPException(status_code=404, detail="Task not found")
     idx = matches[0]
-    tasks[idx] = {
-        "id": id,
-        "task": task,
-        "completed": completed,
-    }
+    tasks[idx] = Task(id=task.id, task=task.task, completed=task.completed)
     return tasks
